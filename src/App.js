1a96c4b1b3bb591e4a78f71d3700843477a28153
Manishpawade <manish.pawade121@gmail.com>
2021-11-10 15:54:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Expense from './components/Expense';
+import Myapp from './components/Myapp';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Myapp/>
+      <Expense/>
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expense.js
@@ -0,0 +1,21 @@
+import './Expense.css';
+
+function Expense() {
+    return (
+        <>
+       
+
+      <div className="expense-item" >
+          <div>31 December 2021</div>
+          <div className="expense-item__description">
+              <h2>Car Insurance </h2>
+              <div className="expense-item__price"> $2999 </div>
+          </div>
+      </div>
+    
+      
+           
+        </>
+    )
+}
+export default Expense;
