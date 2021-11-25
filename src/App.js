72f2d64d588c51f96c2f8c744c0b9ef8a57ae203
Manishpawade <manish.pawade121@gmail.com>
2021-11-25 09:03:50 +0530

    2nd comit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 
-import './App.css';
 import Expense from './components/Expense';
-import Myapp from './components/Myapp';
-
 
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title:'New Mobile',
+      amount: 339,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 499,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 299,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Laptop',
+      amount: 999,
+      date: new Date(2021, 9, 12),
+    },
+  ];
+
   return (
     <div className="container">
-      <Myapp/>
-      <Expense/>
+      <h1 style={{ color: "red", textAlign: "center", fontWeight: "700", padding: "20px" }}>Company Item List</h1>
 
-     
+
+      <Expense title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date} >
+        </Expense>
+
+      <Expense title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}> 
+        </Expense>
+
+      <Expense title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}>
+        </Expense>
+
+      <Expense title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}>
+        </Expense>
+
+
+
+
     </div>
   );
 }
--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,20 +1,21 @@
 import './Expense.css';
+import ExpenseDate from './ExpenseDate';
 
-function Expense() {
+
+function Expense(props) {
+   
+   
     return (
         <>
-       
+         
+            <div className="expense-item" >
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price"> ${props.amount} </div>
+                </div>
+            </div>
 
-      <div className="expense-item" >
-          <div>31 December 2021</div>
-          <div className="expense-item__description">
-              <h2>Car Insurance </h2>
-              <div className="expense-item__price"> $2999 </div>
-          </div>
-      </div>
-    
-      
-           
         </>
     )
 }
